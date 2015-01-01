76e6d97d59773338e72ed8a601d54ae42d765fc2
norman <norman@linode.com>
2015-01-01 10:00:51 +0000

    install go & oracle; tested

--- installgo.sh
+++ installgo.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# sudo apt-get install mercurial bison
-# bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-# source /home/vagrant/.gvm/scripts/gvm
-# gvm install go1.4
-sudo tar zxvf go*.tar.gz -C /usr/local/
-echo "PATH=\"/usr/local/go/bin:\$PATH\"" >> /home/vagrant/.profile
-source /home/vagrant/.profile
+#sudo apt-get install mercurial bison
+#sudo apt-get install gcc binutils make
+#bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+#source /home/vagrant/.gvm/scripts/gvm
+#gvm install go1.4
+
+mkdir -p $HOME/golang/packages
+tar zxvf go*.tar.gz -C $HOME/golang
+echo "PATH=\"$HOME/golang/go/bin:\$PATH\"" >> $HOME/.profile
+echo "export GOROOT=$HOME/golang/go" >> $HOME/.profile
+echo "export GOPATH=$HOME/golang/packages" >> $HOME/.profile
+source $HOME/.profile
+go get code.google.com/p/go.tools/cmd/oracle
