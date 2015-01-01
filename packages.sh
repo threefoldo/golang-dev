5cc4f433a534694c037f2de8ef9d189c35743f35
vagrant <vagrant@ubuntu1404.com>
2015-01-01 04:38:33 +0000

    git config

--- packages.sh
+++ packages.sh
@@ -2,7 +2,7 @@
 
 sudo sed -i 's/us.archive.ubuntu.com/mirrors.163.com/g' /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y screen emacs24 git
+sudo apt-get install -y screen tmux emacs24 git
 sudo apt-get install -y docker.io
 
 git config --global user.name "vagrant"
