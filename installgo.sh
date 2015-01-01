ff9b6fe16f7a77cf6a43c05cb91466109ae037f7
vagrant <vagrant@ubuntu1404.com>
2015-01-01 04:34:48 +0000

    use tar file to install go1.4

--- installgo.sh
+++ installgo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install mercurial bison
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-source /home/vagrant/.gvm/scripts/gvm
-gvm install go1.4
+# sudo apt-get install mercurial bison
+# bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+# source /home/vagrant/.gvm/scripts/gvm
+# gvm install go1.4
+tar zxvf go1.4.linux-amd64.tar.gz
+sudo mv go /usr/local/
+echo "PATH=\"/usr/local/go/bin:\$PATH\"" >> /home/vagrant/.profile
+source /home/vagrant/.profile
